284f0624ce9cb7fa804401b9fa31becdfc32d724
hopemalik <hopemalik0512@gmail.com>
2022-12-20 21:46:52 +0900

    add: totalvolume,sell modal,transfer modal,logout

--- routes/api/mint.js
+++ routes/api/mint.js
@@ -21,9 +21,10 @@ router.post("/mintLvl", (req,res) => {
         console.log((currentTime - deployTime) / 36e5);
         console.log(userAddress);
         console.log(IsWhiteList(userAddress));
-        if(IsWhiteList(userAddress) === 1 &&  ((currentTime - deployTime) / 36e5) <= 24){
-            res.json("whiteList");
-        } else res.json("noWhiteList");
+        // if(IsWhiteList(userAddress) === 1 &&  ((currentTime - deployTime) / 36e5) <= 24){
+        //     res.json("whiteList");
+        // } else res.json("noWhiteList");
+        res.json("noWhiteList");
     } catch(err){
         res.status(404).json("Transaction Error");
     }
--- routes/api/transaction.js
+++ routes/api/transaction.js
@@ -2,11 +2,16 @@ const express = require("express");
 const router = express.Router();
 const Transaction = require('../../models/Transaction');
 const SaveTransaction = require('../save');
-
+const fetch = require('node-fetch');
 // @route   get api/users/test
 // @desc    test User
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "this is test" }));
+
+let totalVolume  = 112;
+let mainMintedCount = 70;
+let sonicMintedCount = 15;
+let mtopMintedCount = 4;
 
 // @route   POST api/transaction
 // @desc    Register User
@@ -14,6 +19,16 @@ router.get("/test", (req, res) => res.json({ msg: "this is test" }));
 router.post("/", (req,res) => {
     try{    
         res.json("success");
+        totalVolume += req.body.amount;
+        if(req.body.collectionId === 1){
+            mainMintedCount++;
+        }
+        if(req.body.collectionId === 3){
+            sonicMintedCount++;
+        }
+        if(req.body.collectionId === 4){
+            mtopMintedCount++;
+        }
         SaveTransaction(req.body);
     } catch(err){
         res.status(404).json("Transaction Error");
@@ -32,14 +47,77 @@ router.post("/getTransactions",async (req,res) => {
         if(sort === "latest"){
             if(game === 1)
                 users = await Transaction.find({userAddress: userAddress,type: type,game:"Crypto 8Ball"}).sort({date: -1});
-        } else 
+        } 
+        if(sort === "oldest") 
         {
             if(game === 1)
                 users = await Transaction.find({userAddress: userAddress,type: type,game:"Crypto 8Ball"}).sort({date: 1});
         }
+        if(sort === "priceMinus"){
+            if(game === 1){
+                users = await Transaction.find({userAddress: userAddress,type: type,game:"Crypto 8Ball"}).sort({amount: -1});
+            }
+        }
+        if(sort === "pricePlus"){
+            if(game === 1){
+                users = await Transaction.find({userAddress: userAddress,type: type,game:"Crypto 8Ball"}).sort({amount: 1});
+            }
+        }
         res.json(users);
     }catch(err) {
         res.status(404).json("find error");
+    }
+})
+
+//get TotalVolume
+router.get('/getTotalVolume',(req,res) => {
+    try{
+        res.json(totalVolume);
+    }catch(err) {
+        res.json(totalVolume);
+    }
+})
+
+//get MainMintedCount
+router.get('/getMainMintedCount',(req,res) => {
+    try{
+        console,log(mainMintedCount);
+        res.json(mainMintedCount);
+    }catch(err) {
+        res.json(mainMintedCount);
+    }
+})
+
+//get SonicMintedCount
+router.get('/getSonicMintedCount',(req,res) => {
+    try{
+        res.json(sonicMintedCount);
+    }catch(err) {
+        res.json(sonicMintedCount);
+    }
+})
+
+//get MtopMintedCount
+router.get('/getMtopMintedCount',(req,res) => {
+    try{
+        res.json(mtopMintedCount);
+    }catch(err) {
+        res.json(mtopMintedCount);
+    }
+})
+
+//get currentAvaxPrice
+router.get('/getCurrentAvaxPrice',async (req,res) => {
+    try{
+        console.log("getCurrentAvaxPrice");
+        let response;
+        await fetch('https://min-api.cryptocompare.com/data/price?fsym=AVAX&tsyms=USD&api_key=f7a6a1d6d3ef3ad2eabc8f0ddf13be828741c5e7e65d43c07aa4d1b0321f41d4')
+            .then(res => res.text())
+            .then(text => response = JSON.parse(text));
+        res.json(response.USD);
+    
+    }catch(err){
+        console.log(err);
     }
 })
 
