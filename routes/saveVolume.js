b29249753772c7901333d7e00ae9fbb6e15f6f29
hopemalik <hopemalik0512@gmail.com>
2023-01-09 23:47:01 +0900

    fixed:in build get the volume from database

--- routes/saveVolume.js
+++ routes/saveVolume.js
@@ -2,35 +2,48 @@ const Volume = require("../models/volume");
 
 const SaveVolume = async ({ type, collectionId, price }) => {
   try {
-    let volume = await Volume.find();
-    console.log(volume);
-    let totalVolume = volume.totalVolume;
-    let mainCount = volume.mainCount;
-    let sonicCount = volume.sonicCount;
-    let mtopCount = volume.mtopCount;
+    let volumes = await Volume.find();
+    let newVolume;
+    let mainCount, sonicCount, mtopCount, totalVolume;
+    if(volumes.length > 0){
+      volumes[volumes.length-1].mainCount === undefined
+        ? (mainCount = 0)
+        : (mainCount = volumes[volumes.length-1].mainCount);
+      volumes[volumes.length-1].sonicCount === undefined
+        ? (sonicCount = 0)
+        : (sonicCount = volumes[volumes.length-1].sonicCount);
+      volumes[volumes.length-1].mtopCount === undefined
+        ? (mtopCount = 0)
+        : (mtopCount = volumes[volumes.length-1].mtopCount);
+      volumes[volumes.length-1].totalVolume === undefined
+        ? (totalVolume = 0)
+        : (totalVolume = volumes[volumes.length-1].totalVolume);
+    } else {
+      mainCount = sonicCount = mtopCount = totalVolume = 0;
+    }
     if (type === "mint") {
-      if (totalVolume === undefined) totalVolume = price;
-      else totalVolume += price;
+      totalVolume += price;
       if (collectionId == 1) {
-        if (mainCount === undefined) mainCount = 1;
-        else mainCount++;
+        mainCount++;
       }
       if (collectionId == 3) {
-        if (sonicCount === undefined) sonicCount = 1;
-        else sonicCount++;
+        sonicCount++;
       }
       if (collectionId == 4) {
-        if (mtopCount === undefined) mtopCount = 1;
-        else mtopCount++;
+        mtopCount++;
       }
     } else {
-      if (totalVolume === undefined) totalVolume = price;
-      else totalVolume += price;
+      totalVolume += price;
     }
-    volume.totalVolume = totalVolume;
-    volume.updateOne
-    console.log("totalVolume===",volume.totalVolume);
-    
+
+    newVolume = new Volume({
+      mainCount: mainCount,
+      sonicCount: sonicCount,
+      mtopCount: mtopCount,
+      totalVolume: totalVolume,
+    });
+    newVolume.save();
+    return;
   } catch (err) {
     console.log(err);
   }
